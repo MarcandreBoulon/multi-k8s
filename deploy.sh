c92b3aa01e57beb803e40a383ae6f8da7d79e6b9
Marc-andre Boulon <Marc-andre.Boulon@uk.ey.com>
2021-04-18 23:56:39 +0100

    final deployments files

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mboulon/multi-client:latest -t mboulon/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mboulon/multi-server:latest -t mboulon/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mboulon/multi-worker:latest -t mboulon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push mboulon/multi-client:latest
+docker push mboulon/multi-server:latest
+docker push mboulon/multi-worker:latest
+
+docker push mboulon/multi-client:$SHA
+docker push mboulon/multi-server:$SHA
+docker push mboulon/multi-worker:$SHA
+
+kubectl -apply -f k8s
+kubectl set image deployments/client-deployment server=mboulon/multi-client:$SHA
+kubectl set image deployments/server-deployment server=mboulon/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=mboulon/multi-worker:$SHA
